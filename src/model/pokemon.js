858b6be9d3afc8f709485696d50da4f72e478381
Juan Diego Jimenez <jdiejim@gmail.com>
2017-04-28 12:46:36 -0600

    add https

--- src/model/pokemon.js
+++ src/model/pokemon.js
@@ -2,7 +2,7 @@
 class Pokemon {
   constructor(id) {
     this.id = id;
-    this.pokeURL = 'http://pokeapi.co/api/v2/pokemon/' + this.id;
+    this.pokeURL = 'https://pokeapi.co/api/v2/pokemon/' + this.id;
     this.sprite = '0 ' + this.id * -106 + 'px';
   }
 }
